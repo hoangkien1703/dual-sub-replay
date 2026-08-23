6866f9e6186fd1451e54039826bc7dbe520cfd2f
hoangkien1703 <92719520+hoangkien1703@users.noreply.github.com>
2026-08-23 19:48:09 +0700

    Fix Compose UI test import

--- app/src/androidTest/java/com/kienhoang/dualsubreplay/ui/LearningPlayerUiTest.kt
+++ app/src/androidTest/java/com/kienhoang/dualsubreplay/ui/LearningPlayerUiTest.kt
@@ -1,6 +1,5 @@
 package com.kienhoang.dualsubreplay.ui
 
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
