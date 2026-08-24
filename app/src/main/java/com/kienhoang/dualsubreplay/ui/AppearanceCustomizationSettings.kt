911e72078ace2c80c289fee8ed2da6e86b207e5c
hoangkien1703 <92719520+hoangkien1703@users.noreply.github.com>
2026-08-25 06:13:31 +0700

    Fix customization settings divider import

--- app/src/main/java/com/kienhoang/dualsubreplay/ui/AppearanceCustomizationSettings.kt
+++ app/src/main/java/com/kienhoang/dualsubreplay/ui/AppearanceCustomizationSettings.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.HorizontalDivider
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
