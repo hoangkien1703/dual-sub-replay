eb35a13cc90e162abc7912eb0f8bb1f5d66f9dd8
hoangkien1703 <92719520+hoangkien1703@users.noreply.github.com>
2026-09-08 06:40:58 +0700

    Prepare official v0.9.9 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
-val appVersionCode = 29
-val appVersionName = "0.9.8"
+val appVersionCode = 30
+val appVersionName = "0.9.9"
 val releaseStoreFile = providers.environmentVariable("ANDROID_RELEASE_STORE_FILE").orNull
 val releaseStorePassword = providers.environmentVariable("ANDROID_RELEASE_STORE_PASSWORD").orNull
 val releaseKeyAlias = providers.environmentVariable("ANDROID_RELEASE_KEY_ALIAS").orNull
